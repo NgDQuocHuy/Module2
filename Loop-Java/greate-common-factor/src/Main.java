d9ff9bfa212aa3e690b9a93863efc3ab4e1fd2d1
QuocHuy <ngdquochuy241@gmail.com>
2022-07-08 14:26:20 +0700

    Bài thực hành 2

---
+++ Loop-Java/greate-common-factor/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+
+        int a;
+        int b;
+        System.out.println("Enter a: ");
+        a = input.nextInt();
+        System.out.println("Enter b: ");
+        b = input.nextInt();
+
+        a = Math.abs(a);
+        b = Math.abs(b);
+
+        if (a == 0 || b == 0) {
+            System.out.println("No greatest common factor");
+        }
+        while (a != b) {
+            if (a > b) {
+                a = a - b;
+            } else {
+                b = b -a;
+            }
+        }
+        System.out.println("Greatest common factor: " + a);
+    }
+}
