812701e1576257b934c2d82c6a491cbb4f3e2834
QuocHuy <ngdquochuy241@gmail.com>
2022-07-08 16:20:39 +0700

    Bài tập 1

--- Loop-Java/menu/src/Main.java
+++ Loop-Java/menu/src/Main.java
@@ -67,11 +67,11 @@ public class Main {
             int height = input.nextInt();
             int k = 0;
             for (int i = 1; i <= height; ++i, k = 0) {
-                for (int j = 1; j <= height - 1; ++j) {
+                for (int j = 1; j <= height - i; ++j) {
                     System.out.print("  ");
                 }
                 while (k != 2 * i -1) {
-                    System.out.print(" * ");
+                    System.out.print("* ");
                     ++k;
                 }
                 System.out.println();
