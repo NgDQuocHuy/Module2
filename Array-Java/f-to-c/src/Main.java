5c730ffdd3d7e7ce2d94e8dbe58f9680dfde420a
QuocHuy <ngdquochuy241@gmail.com>
2022-07-11 16:12:53 +0700

    Độ C and Độ F

---
+++ Array-Java/f-to-c/src/Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        double fahrenheit;
+        double celsius;
+        int choice;
+
+        do {
+            System.out.println("Menu.");
+            System.out.println("1. Fahrenheit to Celsius");
+            System.out.println("2. Celsius to Fahrenheit");
+            System.out.println("0. Exit");
+            System.out.println("Enter your choice: ");
+            choice = input.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter Fahrenheit: ");
+                    fahrenheit = input.nextDouble();
+                    System.out.println("Fahrenheit to Celsius: " + fahrenheitToCelsius(fahrenheit));
+                    break;
+                case 2:
+                    System.out.println("Enter Celsius: ");
+                    celsius = input.nextDouble();
+                    System.out.println("Celsius to Fahrenheit: " + celsiusTofahrenheit(celsius));
+                    break;
+                case 0:
+                    System.exit(0);
+            }
+        } while (choice != 0);
+    }
+
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        double celsius = (5.0 / 9) * (fahrenheit - 32);
+        return  celsius;
+    }
+
+    public static double celsiusTofahrenheit(double celsius) {
+        double fahrenheit = (9.0 / 5) * (celsius + 32);
+        return  fahrenheit;
+    }
+}
+
