a6dc2692e21587dd997d847627c3199d78bf3854
QuocHuy <ngdquochuy241@gmail.com>
2022-07-11 09:07:25 +0700

    Add and Remove

--- exercise-of-aCu/add-and-remove/src/Main.java
+++ exercise-of-aCu/add-and-remove/src/Main.java
@@ -10,25 +10,27 @@ public class Main {
             n = scanner.nextInt();
         } while (n <= 0);
 
-        int A[] = new int[n];
+        int array[] = new int[n];
 
         System.out.println("Nhập các phần tử cho mảng: ");
-        for (i = 0; i < A.length; i++) {
+        for (i = 0; i < array.length; i++) {
             System.out.print("Nhập phần tử thứ " + i + ": ");
-            A[i] = scanner.nextInt();
+            array[i] = scanner.nextInt();
         }
+
+        // hiển thị các phần tử trong mảng
         System.out.println("Mảng hiện tại có các phần tử là: ");
-        for (i = 0; i < A.length; i++) {
-            System.out.print(A[i] + "\t");
+        for (i = 0; i < array.length; i++) {
+            System.out.print(array[i] + "\t");
         }
         System.out.println();
 
         System.out.println("Nhập phần tử muốn xóa khỏi mảng: ");
         int element = scanner.nextInt();
 
-        for (c = i = 0; i < A.length; i++) {
-            if (A[i] != element) {
-                A[c] = A[i];
+        for (c = i = 0; i < array.length; i++) {
+            if (array[i] != element) {
+                array[c] = array[i];
                 c++;
             }
         }
@@ -37,8 +39,8 @@ public class Main {
 
         // hiển thị các phần tử trong mảng sau khi xóa phần từ
         System.out.println("Mảng còn lại sau khi xóa phần tử " + element + " là: ");
-        for (i = 0; i < A.length; i++) {
-            System.out.print(A[i] + "\t");
+        for (i = 0; i < array.length; i++) {
+            System.out.print(array[i] + "\t");
         }
     }
 }
