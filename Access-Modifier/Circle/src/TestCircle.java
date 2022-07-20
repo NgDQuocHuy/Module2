a2028c9b7fc739c5698221d3075ce63fa73b12be
QuocHuy <ngdquochuy241@gmail.com>
2022-07-20 15:48:04 +0700

    Hình Tròn nè

---
+++ Access-Modifier/Circle/src/TestCircle.java
@@ -0,0 +1,7 @@
+public class TestCircle {
+    public static void main(String[] args) {
+    Circle circle = new Circle();
+        System.out.println(circle.getRadius());
+        System.out.println(circle.getColor());
+    }
+}
