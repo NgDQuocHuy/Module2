1554d77b859a762b0778c96ef7dc9efe1b35af57
QuocHuy <ngdquochuy241@gmail.com>
2022-07-20 15:14:07 +0700

    Thuộc Tính Tĩnh

---
+++ Access-Modifier/Static-Property/src/TestStaticProperty.java
@@ -0,0 +1,10 @@
+public class TestStaticProperty {
+    public static void main(String[] args) {
+        Car car1 = new Car("Mazda 3", "Skyactiv 3");
+        System.out.println(Car.numberOfCars);
+        Car car2 = new Car("Mazda 6", "Skyactiv 6");
+        System.out.println(Car.numberOfCars);
+        Car car3 = new Car("Mazda 5", "Skyactiv 5");
+        System.out.println(Car.numberOfCars);
+    }
+}
