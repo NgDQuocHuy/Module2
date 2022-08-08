f603f7dd092a763205df2fb755ffb88fc9ff4098
QuocHuy <ngdquochuy241@gmail.com>
2022-08-08 15:10:54 +0700

    Triển khai Queue use Array

---
+++ Stack-Queue/Implement-Queue/src/MyQueue.java
@@ -0,0 +1,57 @@
+public class MyQueue {
+    public int capacity;
+    public int[] queueArr;
+    public int head = 0;
+    public int tail = -1;
+    public int currentSize = 0;
+
+    public MyQueue(int queueSize) {
+        this.capacity = queueSize;
+        queueArr = new int[this.capacity];
+    }
+
+    public boolean isQueueFull() {
+        boolean status = false;
+        if (currentSize == capacity) {
+            status = true;
+        }
+        return status;
+    }
+
+    public boolean isQueueEmpty() {
+        boolean status = false;
+        if (currentSize == 0) {
+            status = true;
+        }
+        return status;
+    }
+
+    public void enqueue(int item) {
+        if (isQueueFull()) {
+            System.out.println("Overflow ! Unable to add element: " + item);
+        } else {
+            tail++;
+            if (tail == capacity - 1) {
+                tail = 0;
+            }
+            queueArr[tail] = item;
+            currentSize++;
+            System.out.println("Element " + item + " is pushed to Queue !");
+        }
+    }
+
+    public void dequeue() {
+        if (isQueueEmpty()) {
+            System.out.println("Underflow ! Unable to remove element from Queue");
+        } else {
+            head++;
+            if (head == capacity - 1) {
+                System.out.println("Pop operation done ! removed: " + queueArr[head - 1]);
+                head = 0;
+            } else {
+                System.out.println("Pop operation done ! removed: " + queueArr[head - 1]);
+            }
+            currentSize--;
+        }
+    }
+}
