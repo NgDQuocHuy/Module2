a813149e5f623cea35b5e024c90d1d2ff1b6ad79
QuocHuy <ngdquochuy241@gmail.com>
2022-08-08 14:54:05 +0700

    Quản lý sản phẩm

--- Map/Array-Linked-in-Java/src/product/Product.java
+++ Map/Array-Linked-in-Java/src/product/Product.java
@@ -1,25 +1,70 @@
-public class Product {
-    private String name;
-    private int price;
+package product;
 
-    public String getName() {
-        return name;
+import java.lang.Comparable;
+import java.util.Objects;
+
+public class Product{
+    private String idProduct;
+    private String nameProduct;
+    private int priceProduct;
+
+    public Product(String idProduct) {
+        this.idProduct = idProduct;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public Product(String id, String name, int price) {
+        this.idProduct = id;
+        this.nameProduct = name;
+        this.priceProduct = price;
     }
 
-    public int getPrice() {
-        return price;
+    public String getIdProduct() {
+        return idProduct;
     }
 
-    public void setPrice(int price) {
-        this.price = price;
+    public void setIdProduct(String idProduct) {
+        this.idProduct = idProduct;
     }
 
-    public Product(String name, int price) {
-        this.name = name;
-        this.price = price;
+    public String getNameProduct() {
+        return nameProduct;
+    }
+
+    public void setNameProduct(String nameProduct) {
+        this.nameProduct = nameProduct;
+    }
+
+    public int getPriceProduct() {
+        return priceProduct;
+    }
+
+    public void setPriceProduct(int priceProduct) {
+        this.priceProduct = priceProduct;
+    }
+
+    @Override
+    public String toString() {
+        return "Id sản phẩm: "
+                + idProduct
+                + ", Tên sản phẩm: "
+                + nameProduct
+                + ", Giá sản phẩm: " + priceProduct;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Product product = (Product) obj;
+        return idProduct.equals(product.idProduct);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idProduct);
     }
 }
--- Map/Array-Linked-in-Java/src/testProduct/TestProduct.java
+++ Map/Array-Linked-in-Java/src/testProduct/TestProduct.java
@@ -1,2 +1,84 @@
-package testProduct;public class TestProduct {
+package testProduct;
+
+import java.lang.String;
+import product.Product;
+import product.ProductManager;
+
+import java.util.Scanner;
+
+public class TestProduct {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        ProductManager listProducts = new ProductManager();
+        int choice = 0;
+
+        do {
+            System.out.println("* * * * * * * * * * * * *  MENU * * * * * * * * * * * * * * * * * *");
+            System.out.println("*              Lựa chọn chức năng                                 *");
+            System.out.println("*       1.     Thêm sản phẩm                                      *");
+            System.out.println("*       2.     Sửa thông tin sản phẩm theo id                     *");
+            System.out.println("*       3.     Xoá sản phẩm theo id                               *");
+            System.out.println("*       4.     Hiển thị danh sách sản phẩm                        *");
+            System.out.println("*       5.     Tìm kiếm sản phẩm theo tên                         *");
+            System.out.println("*       6.     Sắp xếp sản phẩm tăng dần, giảm dần theo giá       *");
+            System.out.println("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
+            System.out.print("=> Chọn chức năng: ");
+            choice = input.nextInt();
+            input.nextLine();
+            switch (choice) {
+                case 1:
+                    System.out.println("1.     Thêm sản phẩm");
+                    System.out.println("=> Nhập ID sản phẩm:");
+                    String idProduct = input.nextLine();
+                    System.out.println("=> Nhập tên sản phẩm:");
+                    String nameProduct = input.nextLine();
+                    System.out.println("=> Nhập giá sản phẩm:");
+                    int priceProduct = input.nextInt();
+                    Product product = new Product(idProduct, nameProduct, priceProduct);
+                    listProducts.addProduct(product);
+                    break;
+                case 2:
+                    System.out.println("2.     Sửa thông tin sản phẩm theo id");
+                    System.out.print("=> Nhập ID sản phẩm cần sửa: ");
+                    idProduct = input.nextLine();
+                    listProducts.editProduct(idProduct);
+                    break;
+                case 3:
+                    System.out.println("3.     Xoá sản phẩm theo id");
+                    System.out.print("=> Nhập ID sản phẩm: ");
+                    idProduct = input.nextLine();
+                    product = new Product(idProduct);
+                    System.out.println("=> Xóa sản phẩm trong danh sách: " + listProducts.removeProduct(product));
+                    break;
+                case 4:
+                    System.out.println("4.     Danh sách sản phẩm");
+                    listProducts.showListProducts();
+                    break;
+                case 5:
+                    System.out.println("5.     Tìm kiếm sản phẩm theo tên");
+                    System.out.print("=> Nhập tên sản phẩm cần tìm: ");
+                    nameProduct = input.nextLine();
+                    listProducts.findProduct(nameProduct);
+                    break;
+                case 6:
+                    System.out.println("6.     Sắp xếp sản phẩm tăng dần, giảm dần theo giá");
+                    System.out.println("1. Tăng dần");
+                    System.out.println("2. Giảm dần");
+                    System.out.print("=> Chọn chức năng: ");
+                    int choice2 = Integer.parseInt(input.nextLine());
+                    switch (choice2) {
+                        case 1:
+                            System.out.println("=> Danh sách tăng dần:");
+                            listProducts.sortPriceASC();
+                            listProducts.showListProducts();
+                            break;
+                        case 2:
+                            System.out.println("=> Danh sách giảm dần:");
+                            listProducts.sortPriceDESC();
+                            listProducts.showListProducts();
+                            break;
+                    }
+            }
+        } while (choice != 0);
+    }
 }
