78e35bc11b9abc9632f72370c68b0ae7baccf45e
QuocHuy <ngdquochuy241@gmail.com>
2022-08-22 09:10:27 +0700

    Đếm số

---
+++ threading/Count/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        Count count = new Count();
+        try
+        {
+            while(count.getMyThread().isAlive())
+            {
+                System.out.println("Main thread will be alive till the child thread is live");
+                Thread.sleep(1500);
+            }
+        }
+        catch(InterruptedException e)
+        {
+            System.out.println("Main thread interrupted");
+        }
+        System.out.println("Main thread run is over" );
+    }
+}
